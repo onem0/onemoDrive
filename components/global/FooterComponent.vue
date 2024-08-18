5b3381fdc18f9a47e7adff5e5ae3ae1dd748bc1a
neo <kremersneo0@gmail.com>
2024-08-18 18:56:45 +0200

    added GitHub to footer and changed hover color

--- components/global/FooterComponent.vue
+++ components/global/FooterComponent.vue
@@ -2,13 +2,13 @@
   <footer class="bg-gray-900 mt-3 rounded-t-lg">
     <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
       <nav
-        class="-mb-4 columns-2 sm:flex sm:justify-center sm:space-x-12"
+        class="-mb-4 columns-2 flex justify-center space-x-12"
         aria-label="Footer"
       >
         <div v-for="item in navigation.main" :key="item.name" class="pb-6">
           <a
             :href="item.href"
-            class="text-sm leading-6 text-gray-400 hover:text-gray-900"
+            class="text-sm leading-6 text-gray-400 hover:text-gray-300"
             >{{ item.name }}</a
           >
         </div>
@@ -25,6 +25,7 @@ const navigation = {
   main: [
     { name: "Imprint", href: "https://www.onemo.dev/imprint" },
     { name: "Privacy", href: "https://www.onemo.dev/privacy" },
+    { name: "GitHub", href: "https://github.com/onem0/onemoDrive" },
   ],
 };
 </script>
