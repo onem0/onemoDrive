a9c8a347ccb75dcde0af9fe5a0e81a6004a86ee5
neo <kremersneo0@gmail.com>
2024-10-06 18:47:18 +0200

    Added web app support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,37 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      link: [{ rel: "icon", type: "image/png", href: "/favicon.ico" }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.ico",
+        },
+        {
+          rel: "manifest",
+          href: "/manifest/manifest.json",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/icon/logo.png",
+        },
+      ],
+      meta: [
+        {
+          hid: "keywords",
+          name: "keywords",
+          content:
+            "faser app, social media app, microblogging, post short messages, online community, social network, app for short posts, social platform, microblogging platform, app for discussions, share trends, social media interactions, gain followers, create posts, social sharing, social networks, social media exchange, trending topics, user-generated content, social connection",
+        },
+        {
+          name: "apple-mobile-web-app-capable",
+          content: "yes",
+        },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black",
+        },
+      ],
     },
   },
 });
